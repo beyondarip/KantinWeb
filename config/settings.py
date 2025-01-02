295792d7d3d666f0a66ab5301473860b0521c98e
beyondarip <ari2331280@itpln.ac.id>
2025-01-02 19:30:35 +0700

    any 13

--- config/settings.py
+++ config/settings.py
@@ -157,6 +157,7 @@ DEBUG = True
 
 # Static files setup
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
--- config/urls.py
+++ config/urls.py
@@ -31,5 +31,7 @@ urlpatterns = [
     path('', include('merchants.urls')),
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, documents_root=settings.STATIC_ROOT)
